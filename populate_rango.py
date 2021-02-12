2199a0522dba43f60c5120bc84cb8c6ee3b5d839
German Cherstvov <2472866c@student.gla.ac.uk>
2021-02-13 02:28:13 +0500

    fifth commit

--- populate_rango.py
+++ populate_rango.py
@@ -13,7 +13,7 @@ def populate():
     
     for cat, cat_data in cats.items():
         c = add_cat(cat)
-        for p in cat_data['pages','views','likes']:
+        for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
             
     for c in Category.objects.all():
